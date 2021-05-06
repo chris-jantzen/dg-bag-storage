583f72c4139d5374bf3c9f7ee6ea342e668e7aba
Chris <jantzeca@mail.uc.edu>
2021-05-06 10:18:49 -0400

    Disc Model

---
+++ server/models/Disc.ts
@@ -0,0 +1,56 @@
+import { Schema, model } from 'mongoose'
+
+const discSchema = new Schema(
+  {
+    discName: {
+      type: String,
+      required: true,
+    },
+    manufacturer: {
+      type: String,
+      required: false,
+    },
+    plastic: {
+      type: String,
+      required: false,
+    },
+    weight: {
+      type: Number,
+      required: false,
+    },
+    bags: {
+      type: [String],
+      required: false,
+    },
+    speed: {
+      type: Number,
+      required: true,
+    },
+    glide: {
+      type: Number,
+      required: true,
+    },
+    true: {
+      type: Number,
+      required: true,
+    },
+    fade: {
+      type: Number,
+      required: true,
+    },
+    color: {
+      type: String, // Hex?
+      required: true,
+    },
+    notes: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    collection: 'disc',
+    timestamps: true,
+  }
+)
+
+export const Disc = model('disc', discSchema)
