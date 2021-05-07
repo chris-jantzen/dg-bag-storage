046920a51c01dcf692cbeda4c1951dad19e31ff9
Chris <jantzeca@mail.uc.edu>
2021-05-07 13:37:52 -0400

    reorganize controller/repository structure

--- server/controllers/bagController.ts
+++ server/controllers/bagController.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express'
+import { IBag } from '../models/Bag'
 import BagRepository from '../repositories/bagRepository'
 
 class BagController {
@@ -20,21 +21,45 @@ class BagController {
       const { name, discs }: { name: string; discs?: [string] } = req.body
 
       try {
-        await this.repository.createBag(name, discs)
-        res.status(200).send('Success Creating Bag')
+        const bag: IBag | string = await this.repository.createBag(name, discs)
+        return typeof bag === 'string'
+          ? res.status(400).send(bag)
+          : res.status(201).json({ bag })
       } catch (err) {
-        res.status(400).send('Error Creating Bag')
+        res.status(400).send(err.message)
       }
     })
 
     this.router.get('/getAllBags', async (req: Request, res: Response) => {
       try {
-        const bags = await this.repository.getAllBags()
-        res.status(200).json({
-          bags,
-        })
+        const bags: Array<IBag> | null = await this.repository.getAllBags()
+        return !bags ? res.status(404).send('No bags found') : res.status(200).json(bags)
       } catch (err) {
-        console.error(err)
+        res.status(400).send(err.message)
+      }
+    })
+
+    this.router.get('/getBagById/:id', async (req: Request, res: Response) => {
+      const { id } = req.params
+
+      try {
+        const bag: IBag | null = await this.repository.getBagById(id)
+        return !bag
+          ? res.status(404).send('Bag of that Id not found')
+          : res.status(200).json(bag)
+      } catch (err) {
+        res.send(400).send(err.message)
+      }
+    })
+
+    this.router.delete('/delete/:id', async (req: Request, res: Response) => {
+      const { id } = req.params
+
+      try {
+        const message: boolean = await this.repository.deleteBag(id)
+        return message ? res.sendStatus(204) : res.status(404).send('Bag Not Found')
+      } catch (err) {
+        res.send(400).send(err.message)
       }
     })
   }
--- server/repositories/bagRepository.ts
+++ server/repositories/bagRepository.ts
@@ -1,26 +1,48 @@
 import { Bag, IBag } from '../models/Bag'
 
 class BagRepository {
-  public async createBag(name: string, discs?: [string]): Promise<void> {
-    try {
-      await Bag.create({
-        name,
-        discs,
-      })
-    } catch (err) {
-      console.log(err.message)
-    }
+  /**
+   * Create a new bag
+   * @param {string} name Name of the bag
+   * @param {Array<string>} discs List of disc IDs to add to the bag
+   * @returns {IBag} The bag
+   */
+  public async createBag(name: string, discs?: Array<string>): Promise<IBag> {
+    const bag = await Bag.create({
+      name,
+      discs,
+    })
+    return bag
   }
 
-  public async getAllBags(): Promise<Array<IBag> | string> {
-    try {
-      const bagDocuments: Array<IBag> = await Bag.find({})
+  /**
+   * Get all bags
+   * TODO: By User ID
+   * @returns {Array<IBag> | null} Any bags found or null
+   */
+  public async getAllBags(): Promise<Array<IBag> | null> {
+    const bags: Array<IBag> | null = await Bag.find()
+    return bags
+  }
 
-      return bagDocuments
-    } catch (err) {
-      console.log(err.message)
-      return err.message
-    }
+  /**
+   * Get a bag by an Id
+   * @param {string} id
+   * @returns {IBag | null} The bag or a message stating the bag was not found
+   */
+  public async getBagById(id: string): Promise<IBag | null> {
+    const bag: IBag | null = await Bag.findById(id)
+    return bag
+  }
+
+  /**
+   * Delete a bag
+   * @param {string} id
+   * @returns {boolean} True if the bag was deleted, false if bag is not found
+   */
+  public async deleteBag(id: string): Promise<boolean> {
+    const bag: IBag | null = await Bag.findByIdAndDelete(id)
+    return !bag ? false : true
   }
 }
 
