b5ef70cdfc85d3a9208ddebdbd0a0b76007fe81b
Chris <jantzeca@mail.uc.edu>
2021-07-01 17:43:46 -0400

    remove unused type and nvmrc

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { UpdateUserInput, UserLoginInput } from './user.dto';
 import { User, UserDocument, UserModel } from './user.schema';
 
