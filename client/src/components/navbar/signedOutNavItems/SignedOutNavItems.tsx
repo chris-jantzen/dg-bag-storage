b49044041bc2a553c2435556417b1e2c7192cdab
Chris <jantzeca@mail.uc.edu>
2021-08-07 22:43:59 -0400

    Chakrafied navbar

--- client/src/components/navbar/signedOutNavItems/SignedOutNavItems.tsx
+++ client/src/components/navbar/signedOutNavItems/SignedOutNavItems.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { ListItem, Link } from '@chakra-ui/react';
+import { useRedirect } from '../../../hooks/Redirect';
 
 const SignedOutNavItems = () => {
+  const redirect = useRedirect();
   return (
     <>
-      <li>
-        <Link to='#'>Sign Up</Link>
-      </li>
-      <li>
-        <Link to='#'>Log In</Link>
-      </li>
+      <ListItem color='white' fontSize='lg'>
+        <Link
+          onClick={redirect('/signup')}
+          p='0.35em 0.5em'
+          borderRadius='lg'
+          _hover={{ background: '#4797f9' }}
+        >
+          Sign Up
+        </Link>
+      </ListItem>
+      <ListItem color='white' fontSize='lg'>
+        <Link
+          onClick={redirect('/login')}
+          p='0.35em 0.5em'
+          borderRadius='lg'
+          _hover={{ background: '#4797f9' }}
+        >
+          Log In
+        </Link>
+      </ListItem>
     </>
   );
 };
