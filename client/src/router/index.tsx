b464822703c9ffbfff9bc98645dc9b52f93495fe
Chris <jantzeca@mail.uc.edu>
2021-08-14 13:58:07 -0400

    Replace auth context provider

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -9,6 +9,7 @@ import Home from '../views/home/Home';
 import Navbar from '../components/navbar/Navbar';
 import Signup from '../views/signup/Signup';
 import Login from '../views/login/Login';
+import { AuthContextProvider } from '../store/contexts/authContext';
 
 const Welcome = lazy(() => import('../views/welcome/Welcome'));
 
@@ -24,29 +25,31 @@ const IndexRouter: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
-        <Router>
-          <Navbar />
-          <Suspense fallback={<p>Loading...</p>}>
-            <Switch>
-              <Route
-                exact
-                // path={`${process.env.PUBLIC_URL}/`}
-                path='/welcome'
-                component={Welcome}
-              />
-              <BaseRoute path='/' exact={true} />
-              <PrivateRoute path='/home'>
-                <Home />
-              </PrivateRoute>
-              <PublicRoute path='/login'>
-                <Login />
-              </PublicRoute>
-              <PublicRoute path='/signup'>
-                <Signup />
-              </PublicRoute>
-            </Switch>
-          </Suspense>
-        </Router>
+        <AuthContextProvider>
+          <Router>
+            <Navbar />
+            <Suspense fallback={<p>Loading...</p>}>
+              <Switch>
+                <Route
+                  exact
+                  // path={`${process.env.PUBLIC_URL}/`}
+                  path='/welcome'
+                  component={Welcome}
+                />
+                <BaseRoute path='/' exact={true} />
+                <PrivateRoute path='/home'>
+                  <Home />
+                </PrivateRoute>
+                <PublicRoute path='/login'>
+                  <Login />
+                </PublicRoute>
+                <PublicRoute path='/signup'>
+                  <Signup />
+                </PublicRoute>
+              </Switch>
+            </Suspense>
+          </Router>
+        </AuthContextProvider>
       </ChakraProvider>
     </QueryClientProvider>
   );
